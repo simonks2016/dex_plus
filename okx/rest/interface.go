30c339735652b884153154f0e65d61c8a99aa826
simonks@163.com <simonks@163.com>
2026-05-09 19:20:26 +0800

    version 0.1.0

--- okx/rest/interface.go
+++ okx/rest/interface.go
@@ -1,6 +1,9 @@
 package rest
 
-import "github.com/simonks2016/dex_plus/okx/Response"
+import (
+	"github.com/simonks2016/dex_plus/okx/Response"
+	"github.com/simonks2016/dex_plus/okx/param"
+)
 
 type OKXRestAPI interface {
 	GetInstruments(instType string, queryParams ...QueryParam) ([]Response.Instruments, error)
@@ -9,6 +12,6 @@ type OKXRestAPI interface {
 	GetBalance(...QueryParam) ([]Response.AccountBalance, error)
 	GetOrderStatus(instId string, queryParams ...QueryParam) ([]Response.OrderStatus, error)
 
-	PlaceOrder() error
-	CancelOrder() error
+	PlaceOrder(param.PlaceOrderParams) error
+	CancelOrder(param.CancelOrder) error
 }
